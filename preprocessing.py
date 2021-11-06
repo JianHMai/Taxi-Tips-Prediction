6e504802b3f2bb3278ec4bc84d7399684034c0dc
JianHMai <42229566+JianHMai@users.noreply.github.com>
2021-11-06 14:46:47 -0400

    Update dataset and remove 0 and null values

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 def create_subset(df):
+    # Remove all 0 and Null values
+    df = df.loc[~((df['Trip Seconds'] == 0) | (df['Trip Miles'] == 0)| (df['Fare'] == 0) | (df['Trip Seconds'].isna()) | (df['Trip Miles'].isna()) | (df['Fare'].isna()) )]
     # Get a randomized sample of 1%
     df = df.sample(frac = 0.01)
     # Save subset to CSV
